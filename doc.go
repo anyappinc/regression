3fb50c671879cbec87db60216747108dcdae62a9
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2021-11-16 01:04:57 -0800

    Add an empty file just for pkg.go.dev

---
+++ doc.go
@@ -0,0 +1,7 @@
+// package regression is out of date.
+//
+// v2 of regression library is available at https://github.com/anyappinc/regression/tree/master/v2
+//
+// If you want to use the original library, go to https://github.com/sajari/regression.
+
+package regression
